606284c73b6a8997ec5a159f309940f89334c451
KeremEminKati <kerememinkati61@gmail.com>
2026-04-26 13:03:21 +0300

    Solved merge conflict holding two names

--- main.py
+++ main.py
@@ -6,7 +6,11 @@ tech_dictionary = {
  "Git": "A distributed version control system.",
  "Firewall": "A network security system that monitors and controls incoming and outgoing network traffic.",
  "Subnet": "A segmented piece of a larger network.",
- "Docker": "A platform for developing, shipping, and running applications in containers."
+ "Docker": "A platform for developing, shipping, and running applications in containers.",
+
+ "API": "Application Programming Interface, a set of rules for building software applications.",
+ "HTML": "HyperText Markup Language, the standard language for creating web pages.",
+ "CSS": "Cascading Style Sheets, used for describing the presentation of a document"
 }
 def list_terms():
  for term, desc in tech_dictionary.items():
